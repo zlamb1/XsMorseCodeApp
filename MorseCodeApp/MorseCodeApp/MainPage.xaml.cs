8aae87645e41b9436554073eb802c76e1ec177c4
zlamb1 <zlamb@my.wctc.edu>
2023-11-01 14:12:39 -0500

    Add project files.

---
+++ MorseCodeApp/MorseCodeApp/MainPage.xaml.cs
@@ -0,0 +1,83 @@
+﻿using AwsomeApp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MorseCodeApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var halfScreenWidth = Convert.ToInt32(Math.Round(DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density * 0.5));
+            StackLayoutOutput1.WidthRequest = halfScreenWidth;
+            StackLayoutOutput2.WidthRequest = halfScreenWidth;
+        }
+
+        public string[] currentMorseSequence = new string[2];
+        public string[] currentWord = new string[2]; 
+
+        public int GetButtonIndex(Button button)
+        {
+            return int.Parse(button.ClassId);
+        }
+
+        public void SetLabelMorseOutput(string text, int index)
+        {
+            if (index == 0)
+            {
+                LabelMorseOutput1.Text = text;
+            } else
+            {
+                LabelMorseOutput2.Text = text;
+            }
+        }
+
+        public void SetLabelWordOutput(string text, int index)
+        {
+            if (index == 0)
+            {
+                LabelWordOutput1.Text = text;
+            } else
+            {
+                LabelWordOutput2.Text = text;
+            }
+        }
+
+        public void OnClickPeriod(object sender, EventArgs args)
+        {
+            int index = GetButtonIndex(sender as Button);
+            currentMorseSequence[index] += ".";
+            SetLabelMorseOutput(currentMorseSequence[index], index); 
+        }
+
+        public void OnClickDash(object sender, EventArgs args)
+        {
+            int index = GetButtonIndex(sender as Button);
+            currentMorseSequence[index] += "-";
+            SetLabelMorseOutput(currentMorseSequence[index], index);
+        }
+
+        public void OnClickSpace(object sender, EventArgs args)
+        {
+            int index = GetButtonIndex(sender as Button); currentWord[index] += Morse.MorseCoder(currentMorseSequence[index]);
+            currentMorseSequence[index] = "";
+            SetLabelMorseOutput("", index);
+            SetLabelWordOutput(currentWord[index], index);
+        }
+
+        public void ClearWord(object sender, EventArgs args)
+        {
+            int index = GetButtonIndex(sender as Button); currentMorseSequence[index] = "";
+            currentWord[index] = "";
+            SetLabelMorseOutput("", index);
+            SetLabelWordOutput("", index);
+        }
+    }
+}
---
+++ MorseCodeApp/MorseCodeApp/Morse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace AwsomeApp
+{
+    public class Morse
+    {
+        private static string[] codes = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "" };
+
+        private static char[] letters = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '};
+        public Morse()
+        {
+
+
+        }
+        public static char MorseCoder(string code) {
+            char result='?';
+            for (int i = 0; i < codes.Length;i++) {
+                if (codes[i].Equals(code)) {
+                    result = letters[i];
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
